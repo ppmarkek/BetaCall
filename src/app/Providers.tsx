c613055db33f210b9832a8f0769279b9ecd83926
markek <markungeitis01@gmail.com>
2025-02-21 22:31:58 +0200

    Update Providers.tsx

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,13 +1,8 @@
 'use client';
 
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
-import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
 
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <SessionProvider>
-      <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>
-    </SessionProvider>
-  );
+  return <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>;
 }
