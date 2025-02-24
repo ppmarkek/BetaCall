313c3d68fad50bd16a251402c7caa3bba037d0a6
ppmarkek <153396079+ppmarkek@users.noreply.github.com>
2025-02-24 20:42:44 +0200

    feat: check Verification

--- src/app/verify/[email]/page.tsx
+++ src/app/verify/[email]/page.tsx
@@ -1,23 +1,59 @@
 'use client';
 
 import { useParams } from 'next/navigation';
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex, Image, Spinner } from '@chakra-ui/react';
 import Typography from '@/components/typography/typography';
 import { StyledLink } from '../style';
 import { resendVerification } from '@/app/api/auth/route';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const VerifyEmail = () => {
   const { email } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [beenVerified, setBeenVerified] = useState(false);
 
   const decodedEmail =
     typeof email === 'string' ? decodeURIComponent(email) : '';
 
+  const handleResendVerification = async (
+    emailToVerify: string | string[]
+  ): Promise<void> => {
+    try {
+      if (emailToVerify) {
+        const response = await resendVerification(
+          Array.isArray(emailToVerify) ? emailToVerify[0] : emailToVerify
+        );
+        if (response.status === 400) {
+          setBeenVerified(true);
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     if (decodedEmail) {
-      resendVerification(decodedEmail);
+      handleResendVerification(decodedEmail);
+    } else {
+      setLoading(false);
     }
   }, [decodedEmail]);
+
+  if (loading) {
+    return (
+      <Flex
+        height={'calc(100svh - 81px)'}
+        minHeight={'700px'}
+        alignItems={'center'}
+        justifyContent={'center'}
+      >
+        <Spinner size="xl" />
+      </Flex>
+    );
+  }
 
   return (
     <Flex
@@ -29,19 +65,27 @@ const VerifyEmail = () => {
       <Flex flexDirection="column" gap="50px" alignItems="center">
         <Image src="/assets/recover.png" alt="Confirmation" />
         <Flex flexDirection="column" textAlign="center" width="450px" gap="5px">
-          <Typography variant="H1">
-            We have sent a confirmation email to your address.
-          </Typography>
-          <Typography variant="Regular" color="#8083A3">
-            <br /> Please check your email.
-            <br /> If the email does not arrive, click “Resend email.”
-          </Typography>
+          {beenVerified ? (
+            <Typography variant="H1">Account verified successfully</Typography>
+          ) : (
+            <>
+              <Typography variant="H1">
+                We have sent a confirmation email to your address.
+              </Typography>
+              <Typography variant="Regular" color="#8083A3">
+                <br /> Please check your email.
+                <br /> If the email does not arrive, click “Resend email.”
+              </Typography>
+            </>
+          )}
         </Flex>
-        <StyledLink
-          onClick={() => decodedEmail && resendVerification(decodedEmail)}
-        >
-          Resend Email
-        </StyledLink>
+        {beenVerified ? (
+          <StyledLink onClick={() => handleResendVerification(decodedEmail)}>
+            Resend Email
+          </StyledLink>
+        ) : (
+          <StyledLink href="/signIn">Sign In</StyledLink>
+        )}
       </Flex>
     </Flex>
   );
--- src/app/verify/token/[token]/page.tsx
+++ src/app/verify/token/[token]/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, useSearchParams } from 'next/navigation';
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex, Image, Spinner } from '@chakra-ui/react';
 import Typography from '@/components/typography/typography';
 import { StyledLink } from '../../style';
 import { resendVerification } from '@/app/api/auth/route';
@@ -13,14 +13,49 @@ const VerifyEmail = () => {
   const email = searchParams.get('email');
   const { token } = useParams();
   const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [beenVerified, setBeenVerified] = useState(false);
 
   useEffect(() => {
     if (!token) return;
     axios
       .get(`https://betacall-backend.onrender.com/api/users/verify/${token}`)
-      .then((res) => setMessage(res.data.message))
-      .catch((err) => setMessage(err.response.data.message));
+      .then((res) => {
+        setMessage(res.data.message);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setMessage(err.response.data.message);
+        setLoading(false);
+      })
+      .finally(() => setLoading(false));
   }, [token]);
+
+  if (loading) {
+    return (
+      <Flex
+        height={'calc(100svh - 81px)'}
+        minHeight={'700px'}
+        alignItems={'center'}
+        justifyContent={'center'}
+      >
+        <Spinner size="xl" />
+      </Flex>
+    );
+  }
+
+  const handleResendVerification = async () => {
+    try {
+      if (email) {
+        const response = await resendVerification(email);
+        if (response.status === 400) {
+          setBeenVerified(true);
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <Flex
@@ -32,13 +67,16 @@ const VerifyEmail = () => {
       <Flex flexDirection="column" gap="50px" alignItems="center">
         <Image src="/assets/recover.png" alt="Confirmation" />
         <Flex flexDirection="column" textAlign="center" gap="5px">
-          <Typography variant="H1">{message}</Typography>
+          {beenVerified ? (
+            <Typography variant="H1">Account verified successfully</Typography>
+          ) : (
+            <Typography variant="H1">{message}</Typography>
+          )}
         </Flex>
-        {message === 'Account verified successfully' && (
-          <StyledLink href="/signIn">Sign In</StyledLink>
-        )}
-        {message === 'The link is invalid or expired' && (
-          <StyledLink onClick={() => email && resendVerification(email)}>
+        {message === 'Account verified successfully' ||
+          (beenVerified && <StyledLink href="/signIn">Sign In</StyledLink>)}
+        {message === 'The link is invalid or expired' && !beenVerified && (
+          <StyledLink onClick={() => handleResendVerification()}>
             Resend Email
           </StyledLink>
         )}
