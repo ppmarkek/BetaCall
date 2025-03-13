1f4b16855205f86bdc5d7196007d77427a21ac26
ppmarkek <153396079+ppmarkek@users.noreply.github.com>
2025-03-13 18:24:49 +0200

    Update appwrite.ts

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -4,7 +4,7 @@ const client = new Client();
 
 client
   .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('67b999f8003632e8beb2'); 
+  .setProject('67d3036900280982955e'); 
 
 const account = new Account(client);
 
